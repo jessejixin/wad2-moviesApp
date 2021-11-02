a06e2b4476bf2aefbe90c7c0de25c792ea901851
Xin Ji <20096422@mail.wit.ie>
2021-11-02 10:48:44 +0000

    E2E tests - Home page - Filtering by the title

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,12 +1,63 @@
-describe("Home Page", () => {
-    beforeEach(() => {
-      cy.visit("/");
-    });
-  
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+describe("Home Page ", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+  beforeEach(() => {
+    cy.visit("/")
+  });
     describe("Base test", () => {
       it("displays page header", () => {
         cy.get("h3").contains("Discover Movies");
         cy.get("h1").contains("Filter the movies");
       });
     });
-  })
+    describe("Filtering", () => {
+        describe("By movie title", () => {
+         it("should only display movies with m in the title", () => {
+           let searchString = "m";
+           let matchingMovies = filterByTitle(movies, searchString);
+           cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         })
+         it("should only display movies with o in the title", () => {
+           let searchString = "o";
+           let matchingMovies = filterByTitle(movies, searchString);
+           cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         });
+       })
+       describe("By movie genre" ,() => {
+         // More later
+       })
+     })
+   })
