076c0924634071256fe925d44a2d25fbdee713d8
Xin Ji <20096422@mail.wit.ie>
2021-11-03 22:16:58 +0000

    E2E tests - Home page - Filtering by title and genre.

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -81,5 +81,24 @@ describe("Home Page ", () => {
           });
         });
       });
+       describe("By movie title and genre", () => {
+          it("should display movies with the specified genre and m in the title", () => {
+           let searchString = "f";
+           let matchingTitleMovies = filterByTitle(movies, searchString);
+           const selectedGenreId = 35;
+           const selectedGenreText = "Comedy";
+           const matchingMovies = filterByGenre(matchingTitleMovies, selectedGenreId);
+           cy.get("#filled-search").clear().type(searchString); // Enter f in text box
+           cy.get("#genre-select").click();
+           cy.get("li").contains(selectedGenreText).click();
+           cy.get(".MuiCardHeader-content").should(
+            "have.length",
+            matchingMovies.length
+          );
+          cy.get(".MuiCardHeader-content").each(($card, index) => {
+            cy.wrap($card).find("p").contains(matchingMovies[index].title);
+          });
+          });
     });
   });
+})
