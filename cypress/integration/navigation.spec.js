50f02f16c82aa6daa25da8954a84569e0e81b965
Xin Ji <20096422@mail.wit.ie>
2021-11-09 12:32:47 +0000

    E2E tests - Navigation - Backward/Forward between the home page and movie details page.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -55,6 +55,19 @@ describe("Navigation", () => {
           cy.get("h3").contains(movies[0].title);
         });
       });
+      describe("The forward/backward links", () => {
+        beforeEach(() => {
+          cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+        });
+        it("should navigate backward and forward between the movies detail page and the Discover page.", () => {
+          cy.get("button[aria-label='go back'").click();
+          cy.get("h3").contains("Discover Movies");
+          cy.url().should("not.include", `/movies/${movies[0].id}`);
+          cy.get("button[aria-label='go forward'").click();
+          cy.url().should("include", `/movies/${movies[0].id}`);
+          cy.get("h3").contains(movies[0].title);
+        });
+      });
     describe(
       "when the viewport is a mobile",
       {
