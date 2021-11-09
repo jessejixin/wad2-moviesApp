b4a8aaba83947d9866303373e071dbf2354af3be
Xin Ji <20096422@mail.wit.ie>
2021-11-09 13:00:55 +0000

    E2E tests - Home page - Select favourite movie functionality Favourites,Bug Fixed.

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -100,30 +100,22 @@ describe("Home Page ", () => {
           });
           });
     });
-    describe("Selecting favourites", () => {
-      it("should display an avatar for tagged movies and list them on the Favourites page", () => {
-        cy.get(".MuiAvatar-circle").should(
-          "have.length",
-          0
-        );   
-        cy.get("button[aria-label='add to favorites']").eq(0).click();
-        cy.get("button[aria-label='add to favorites']").eq(2).click();
-        cy.get(".MuiAvatar-circle").should(
-          "have.length",
-          2
-        );  
-        // Are correct cards tagged?
-        cy.get(".MuiCardHeader-root").eq(0).find(".MuiAvatar-circle")    
-        cy.get(".MuiCardHeader-root").eq(2).find(".MuiAvatar-circle")    
-        // Check the Favourites page.
-        cy.get("header").find(".MuiToolbar-root").find("button").eq(1).click();
-        cy.get(".MuiCardHeader-content").should(
-          "have.length",
-          2
-        );
-        cy.get(".MuiCardHeader-content").eq(0).find("p").contains(movies[0].title)
-        cy.get(".MuiCardHeader-content").eq(1).find("p").contains(movies[2].title)
-      });
+  });
+  describe("select and add favourite movie", () => {
+    beforeEach(() => {
+      cy.get("button[aria-label='add to favorites']").eq(0).click();
+      cy.get("button[aria-label='add to favorites']").eq(1).click();
+      cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();
+    });
+    it("should display an avatar at the top of the movie card and add favourite movie to the Favourite movies page", () => {
+      cy.get(".MuiCardHeader-avatar").eq(0).click();
+      cy.get(".MuiCardHeader-avatar").eq(1).click();
+      cy.url().should("include", `/favorites`);
+      cy.get(".MuiCardHeader-content").contains(movies[0].title);
+
+      // cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+      // cy.url().should("include", `/movies/${movies[0].id}`);
+      // cy.get("h3").contains(movies[0].title);
+    });
     });
   });
-})
