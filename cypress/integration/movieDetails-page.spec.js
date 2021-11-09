f4afd468ae3024a2118428889dba8cb448d2e26d
Xin Ji <20096422@mail.wit.ie>
2021-11-09 12:24:47 +0000

    E2E tests - Movie Details page - poster images.

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -1,6 +1,7 @@
 let movieId = 335983; // The movie Venom
 let movie;
 let reviews;
+let images;
 
 describe("Movie Details Page", () => {
   before(() => {
@@ -14,30 +15,47 @@ describe("Movie Details Page", () => {
         movie = movieDetails;
         return movieDetails.id;
       });
+    
+      cy.request(
+        `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${Cypress.env(
+          "TMDB_KEY"
+        )}`
+      ) 
+      .its("body")
+      .then((movieImages) => {
+        images = movieImages;
+        return movieImages.id;
+      });
   });
   beforeEach(() => {
     cy.visit(`/movies/${movie.id}`);
   });
   describe("Base tests", () => {
     it("should display movie title in the page header", () => {
-        cy.get("h3").contains(movie.title);
+      cy.get("h3").contains(movie.title);
     });
 
     it("should display the movie's details", () => {
-      cy.get("h3").contains("Overview");
-      cy.get("h3").next().contains(movie.overview);
-      cy.get("ul")
-        .eq(1)
-        .within(() => {
-          const genreChips = movie.genres.map((g) => g.name);
-          genreChips.unshift("Genres");
-          cy.get("span").each(($card, index) => {
-            cy.wrap($card).contains(genreChips[index]);
-          });
+        cy.get("h3").contains("Overview");
+        cy.get("h3").next().contains(movie.overview);
+        cy.get("ul")
+          .eq(1)
+          .within(() => {
+            const genreChips = movie.genres.map((g) => g.name);
+            genreChips.unshift("Genres");
+            cy.get("span").each(($card, index) => {
+              cy.wrap($card).contains(genreChips[index]);
+            });
         });
     });
-    it("should display the movie posters on the left-hand side", ()=>{
-      cy.get()
-    });
-    });
+  
+    it("should display the movie's posters on the left-hand side", ()=>{
+      const posterLinks = images.posters.map((i) => i.file_path);
+      console.log(posterLinks);
+      cy.get("img").should("have.length", posterLinks.length);
+      cy.get("img").each(($img, index)=> {
+          cy.wrap($img).should('have.attr','src','https://image.tmdb.org/t/p/w500/'+posterLinks[index]);
+      });
   });
+});
+});
