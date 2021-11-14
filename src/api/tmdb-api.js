2ab9dbb6d3a9674a8e9ab639d011c507401b90da
Xin Ji <20096422@mail.wit.ie>
2021-11-14 02:25:56 +0000

    Fix upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -73,11 +73,16 @@ export const getMovie = (args) => {
       });
   };
 
-  export const getUpcomingMoviesPage = ()=> {
+  export const getUpcomingMovies = ()=> {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };
-  
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+         throw error
+      });
+    };
--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ import AddMovieReviewPage from './pages/addMovieReviewPage';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 360000,
-      refetchInterval: 360000, 
+      staleTime: 1,
+      refetchInterval: 1, 
       refetchOnWindowFocus: false
     },
   },
